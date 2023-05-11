c003bb88bcf0cda197cc9e0984f8230232884aba
ltroy404 <62096720+ltroy404@users.noreply.github.com>
2023-05-11 14:56:01 +0300

    Add files via upload
    v1.0

---
+++ bot.py
@@ -0,0 +1,198 @@
+import logging
+from telegram import Update
+import socket
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, CallbackQueryHandler
+import requests
+
+API_SERVER_URL = "http://localhost:12345"
+TOKEN = "6194979947:AAGcZCLLZ-UB96dzxwBywAH3aAaPTEsaB5k"
+
+def add_api_key(update: Update, context: CallbackContext):
+    if context.user_data.get('state') != 'adding_api_key':
+        handle_unexpected_text(update, context)
+        return
+
+    api_key = update.message.text
+    context.user_data['api_key'] = api_key
+    context.user_data['state'] = None
+    update.message.reply_text("API ключ успешно добавлен!")
+
+
+def print_unanswered_feedbacks(context):
+    try:
+        feedbacks = get_unanswered_feedbacks(context)
+
+        if feedbacks and not feedbacks.get("error"):
+            print("Неотвеченные отзывы:")
+            for feedback in feedbacks["data"]["feedbacks"]:
+                print(feedback)
+        else:
+            print("Ошибка при получении данных с сервера:", feedbacks.get("message"))
+    except Exception as e:
+        print(f"Ошибка: {str(e)}, свяжитесь с поддержкой: @ltroy_sw")
+
+
+def get_unanswered_feedbacks(context, take=10, skip=0):
+    try:
+        api_key = context.user_data.get('api_key')
+        if not api_key:
+            return {"error": True, "message": "Отсутствует API для запроса отзывов. Пожалуйста, добавьте ключ."}
+        headers = {"Authorization": f"Bearer {api_key}"}
+        response = requests.get(f"{API_SERVER_URL}/api/v1/feedbacks/unanswered", params={"take": take, "skip": skip}, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {"error": True, "message": f"Error fetching data from server. Status code: {response.status_code}, Response: {response.text}"}
+    except requests.exceptions.RequestException as e:
+        return {"error": True, "message": f"Ошибка: {str(e)}, свяжитесь с поддержкой: @ltroy_sw"}
+
+
+def send_response_to_review(api_key, review_id, response_text):
+    try:
+        headers = {"Authorization": f"Bearer {api_key}"}
+        response = requests.post(f"{API_SERVER_URL}/api/v1/feedbacks/{review_id}/reply", json={"response": response_text}, headers=headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {"error": True, "message": f"Ошибка отправки ответа на сервер. Пожалуйста, свяжитесь с поддержкой: @ltroy_sw Status code: {response.status_code}, Response: {response.text}"}
+    except Exception as e:
+        return {"error": True, "message": f"Ошибка: {str(e)}, свяжитесь с поддержкой: @ltroy_sw"}
+
+
+def start(update: Update, context: CallbackContext):
+    try:
+        keyboard = [
+            [InlineKeyboardButton("Настройки", callback_data="settings")],
+            [InlineKeyboardButton("Неотвеченные отзывы", callback_data='unanswered_feedbacks')],
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        update.message.reply_text('Привет! Я бот проекта FeedBackGuru. Выберите действие:', reply_markup=reply_markup)
+    except Exception as e:
+        print(f"Ошибка: {str(e)}, свяжитесь с поддержкой: @ltroy_sw")
+        update.message.reply_text("Произошла ошибка, пожалуйста, свяжитесь с поддержкой: @ltroy_sw")
+
+
+
+def send_message_to_server(prompt):
+    try:
+        response = requests.post(f"{API_SERVER_URL}/api/v1/generate_response", json={"prompt": prompt})
+        if response.status_code == 200:
+            return response.json()["response"]
+        else:
+            print(f"Ошибка сервера: {response.status_code}, {response.text}")
+            return "Ошибка при обработке запроса на сервере"
+    except Exception as e:
+        print(f"Ошибка: {str(e)}")
+        return f"Ошибка: {str(e)}, свяжитесь с поддержкой: @ltroy_sw"
+
+def button_callback(update, context):
+    try:
+        query = update.callback_query
+        query.answer()
+
+        keyboard = []
+
+        if query.data == 'settings':
+            keyboard = [
+                [InlineKeyboardButton("Добавить API Wildberries", callback_data="add_wildberries_api_key")],
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            query.edit_message_text(text="Настройки:", reply_markup=reply_markup)
+
+        if query.data == 'add_wildberries_api_key':
+            context.user_data['state'] = 'adding_api_key'
+            query.edit_message_text(text="Отправьте свой ключ Wildberries")
+
+        if query.data == 'unanswered_feedbacks':
+            feedbacks = get_unanswered_feedbacks(context)
+            if feedbacks and not feedbacks.get("error"):
+                if feedbacks["data"]["feedbacks"]:
+                    for feedback in feedbacks["data"]["feedbacks"]:
+                        context.user_data['feedback_text'] = feedback["text"]
+                        keyboard = [[InlineKeyboardButton("Сгенерировать ответ", callback_data=f'generate_response:{feedback["id"]}')]]
+                        reply_markup = InlineKeyboardMarkup(keyboard)
+                        update.callback_query.message.reply_text(feedback["text"], reply_markup=reply_markup)
+                else:
+                    update.callback_query.message.reply_text("Нет неотвеченных отзывов")
+            else:
+                query.edit_message_text(text=feedbacks.get("message"))
+
+        elif query.data.startswith('generate_response:'):
+            feedback_id = query.data.split(':')[1]
+            prompt = context.user_data.get('feedback_text')
+            generated_response = send_message_to_server(prompt)
+            keyboard = [
+                [InlineKeyboardButton("Опубликовать ответ", callback_data=f'publish_response:{feedback_id}')],
+                [InlineKeyboardButton("Исправить и опубликовать", callback_data=f'correct_and_publish_response:{feedback_id}')],
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            query.edit_message_text(text=generated_response, reply_markup=reply_markup)
+
+        elif query.data.startswith('publish_response:'):
+            feedback_id = query.data.split(':')[1]
+            response_text = query.message.text
+            api_key = context.user_data.get('api_key')
+            result = send_response_to_review(api_key, feedback_id, response_text)
+
+            if result.get("error"):
+                query.edit_message_text(text=f"Ошибка при отправке ответа: {result.get('message')}")
+            else:
+                query.edit_message_text(text="Ответ опубликован")
+
+
+        elif query.data.startswith('correct_and_publish_response:'):
+            feedback_id = query.data.split(':')[1]
+            context.user_data['current_feedback_id'] = feedback_id
+            context.user_data['state'] = 'editing_response'
+            query.edit_message_text(text="Пожалуйста, внесите изменения в сгенерированный ответ и отправьте его обратно.")
+
+    except Exception as e:
+        print(f"Ошибка: {str(e)}, свяжитесь с поддержкой: @ltroy_sw")
+        query.edit_message_text("Произошла ошибка, пожалуйста, свяжитесь с поддержкой: @ltroy_sw")
+
+
+def send_edited_response_to_review(update: Update, context: CallbackContext):
+    if context.user_data.get('state') != 'editing_response':
+        handle_unexpected_text(update, context)
+        return
+
+    edited_response = update.message.text
+    feedback_id = context.user_data.get('current_feedback_id')
+    api_key = context.user_data.get('api_key')
+    result = send_response_to_review(api_key, feedback_id, edited_response)
+
+    if result.get("error"):
+        update.message.reply_text(f"Ошибка при отправке исправленного ответа: {result.get('message')}")
+    else:
+        update.message.reply_text("Исправленный ответ опубликован")
+        context.user_data['state'] = None
+
+def handle_unexpected_text(update: Update, context: CallbackContext):
+    if context.user_data.get('state') == 'editing_response':
+        send_edited_response_to_review(update, context)
+    else:
+        update.message.reply_text("Пожалуйста, нажмите на соответствующую функцию, чтобы клиент обработал сообщение. Если возникла проблема или по любым вопросам пишите в поддержку: @ltroy_sw")
+
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CallbackQueryHandler(button_callback))
+    dp.add_handler(MessageHandler(Filters.text & Filters.chat_type.private & ~Filters.command & Filters.update.message, add_api_key))
+    dp.add_handler(MessageHandler(Filters.text & Filters.chat_type.private & ~Filters.command & Filters.update.edited_message, handle_unexpected_text))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_unexpected_text))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+    main()
---
+++ server.py
@@ -0,0 +1,114 @@
+import socket
+import openai
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+WB_API_URL = "https://feedbacks-api.wildberries.ru"
+openai.api_key = "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"
+
+@app.route("/api/v1/feedbacks/unanswered", methods=["GET"])
+def get_unanswered_feedbacks():
+    try:
+        api_key = request.headers.get("Authorization")
+        if not api_key:
+            return jsonify({"error": True, "message": "API-ключ не предоставлен"}), 400
+
+        take = request.args.get("take", 10)
+        skip = request.args.get("skip", 0)
+        headers = {"Authorization": api_key, "accept": "application/json"}
+        params = {"take": take, "skip": skip, "answered": "false", "isAnswered": "false"}
+        response = requests.get(f"{WB_API_URL}/api/v1/feedbacks", headers=headers, params=params)
+
+        if response.status_code == 200:
+            return jsonify(response.json())
+        else:
+            print(f"Error fetching data from Wildberries API. Status code: {response.status_code}, Response: {response.text}")
+
+        return jsonify({"error": True, "message": "Error fetching data from Wildberries API"}), 500
+    except Exception as e:
+        print(f"Ошибка: {str(e)}, свяжитесь с поддержкой: @ltroy_sw")
+        return jsonify({"error": True, "message": "Произошла ошибка, пожалуйста, свяжитесь с поддержкой: @ltroy_sw"}), 500
+
+
+
+def reply_to_review(api_key, review_id, reply_text):
+    url = "https://feedbacks-api.wildberries.ru/api/v1/feedbacks"
+    headers = {
+        "Authorization": api_key,
+        "Content-Type": "application/json",
+        "accept": "*/*"
+    }
+    data = {
+        "id": review_id,
+        "text": reply_text
+    }
+
+    response = requests.patch(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Ошибка при отправке ответа на отзыв: {response.status_code}, пожалуйста, свяжитесь с поддержкой: @ltroy_sw")
+        return None
+
+
+@app.route("/api/v1/generate_response", methods=["POST"])
+def generate_response():
+    prompt = request.json.get("prompt")
+    if not prompt:
+        return jsonify({"error": True, "message": "Требуется указать текст для генерации ответа"}), 400
+
+    response = generate_gpt3_response(prompt)
+    return jsonify({"response": response})
+
+
+def generate_gpt3_response(prompt):
+    content = f"Ответь на отзыв клиента: {prompt}"
+
+    messages = [
+        {"role": "user", "content": content}
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0301",
+        messages=messages,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    return response.choices[0].message['content'].strip()
+
+
+@app.route("/api/v1/feedbacks/<review_id>/reply", methods=["POST"])
+def post_feedback_reply(review_id):
+    api_key = request.headers.get("Authorization")
+    if not api_key:
+        return jsonify({"error": True, "message": "API-ключ не предоставлен"}), 400
+
+    response_text = request.json.get("response")
+    if not response_text:
+        return jsonify({"error": True, "message": "Response is required"}), 400
+
+    result = reply_to_review(api_key, review_id, response_text)
+
+    if result:
+        return jsonify({"message": "Reply sent successfully"})
+    else:
+        return jsonify({"error": True, "message": "Error sending reply to review"}), 500
+
+
+
+def main():
+    app.run(host="localhost", port=12345, threaded=True)
+
+if __name__ == "__main__":
+    main()
