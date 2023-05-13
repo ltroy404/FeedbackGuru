694093864c2496c5213d28906e98f1a35f1f9d96
ltroy404 <62096720+ltroy404@users.noreply.github.com>
2023-05-13 18:07:41 +0300

    v1.3.1 — перенесена работа с БД на сервер, добавлены пояснения

--- server.py
+++ server.py
@@ -12,21 +12,8 @@ WB_API_URL = "https://feedbacks-api.wildberries.ru"
 openai.api_key = "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"
 
 
-def get_api_key(user_id: int):
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-
-    # Получение API ключа пользователя
-    c.execute("SELECT api_key FROM users WHERE user_id=?", (user_id,))
-    api_key = c.fetchone()  # Извлечение API ключа из результата запроса
-    conn.close()
-
-    if api_key:
-        return api_key[0]
-    else:
-        return None
-
-def create_table_if_not_exists():
+#БЛОК РАБОТЫ С БД
+def create_table_if_not_exists():#создание БД
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
 
@@ -41,8 +28,22 @@ def create_table_if_not_exists():
     conn.commit()
     conn.close()
 
+def get_api_key(user_id: int):#копирует API WB из БД
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+
+    # Получение API ключа пользователя
+    c.execute("SELECT api_key FROM users WHERE user_id=?", (user_id,))
+    api_key = c.fetchone()  # Извлечение API ключа из результата запроса
+    conn.close()
+
+    if api_key:
+        return api_key[0]
+    else:
+        return None
+
 @app.route("/api/v1/add_api_key", methods=["POST"])
-def receive_api_key():
+def receive_api_key():#принимает API WB и вызывает add_api_key
     api_key = request.json.get("api_key")
     user_id = request.json.get("user_id")
     if not api_key or not user_id:
@@ -52,7 +53,7 @@ def receive_api_key():
 
     return jsonify({"message": "API-ключ успешно получен"})
 
-def add_api_key(user_id: int, api_key: str):
+def add_api_key(user_id: int, api_key: str):#добавляет API WB в БД
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
 
@@ -71,7 +72,7 @@ def add_api_key(user_id: int, api_key: str):
     print(get_api_key(user_id))
 
 @app.route("/api/v1/remove_api_key", methods=["POST"])
-def receive_user_id_for_removal():
+def receive_user_id_for_removal():#принимает user_id и вызывает remove_api_key
     user_id = request.json.get("user_id")
     if not user_id:
         return jsonify({"error": True, "message": "User_id is required"}), 400
@@ -80,7 +81,7 @@ def receive_user_id_for_removal():
 
     return jsonify({"message": "API key removed successfully"})
 
-def remove_api_key(user_id: int):
+def remove_api_key(user_id: int):#удаляет API WB из БД
     print(get_api_key(user_id))
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
@@ -105,8 +106,10 @@ def remove_api_key(user_id: int):
     else:
         return None  # Если пользователь не найден в базе данных
 
+
+#БЛОК РАБОТЫ С WB
 @app.route("/api/v1/feedbacks/unanswered", methods=["GET"])
-def get_unanswered_feedbacks():
+def get_unanswered_feedbacks():  # Обрабатывает запрос на получение непрошедших проверку отзывов от клиента.
     try:
         user_id = request.headers.get("user_id")
         if not user_id:
@@ -118,21 +121,51 @@ def get_unanswered_feedbacks():
 
         take = request.args.get("take", 10)
         skip = request.args.get("skip", 0)
-        headers = {"Authorization": api_key, "accept": "application/json"}
-        params = {"take": take, "skip": skip, "answered": "false", "isAnswered": "false"}
-        response = requests.get(f"{WB_API_URL}/api/v1/feedbacks", headers=headers, params=params)
 
-        if response.status_code == 200:
-            return jsonify(response.json())
+        response = fetch_unanswered_feedbacks(api_key, take, skip)
+        if response:
+            return jsonify(response)
         else:
-            print(f"Error fetching data from Wildberries API. Status code: {response.status_code}, Response: {response.text}")
+            return jsonify({"error": True, "message": "Error fetching data from Wildberries API"}), 500
 
-        return jsonify({"error": True, "message": "Error fetching data from Wildberries API"}), 500
     except Exception as e:
         print(f"Ошибка: {str(e)}, свяжитесь с поддержкой: @ltroy_sw")
         return jsonify({"error": True, "message": "Произошла ошибка, пожалуйста, свяжитесь с поддержкой: @ltroy_sw"}), 500
 
-def reply_to_review(api_key, review_id, reply_text):
+
+def fetch_unanswered_feedbacks(api_key, take, skip):  # Получает непроверенные отзывы с использованием API WB и возвращает результат.
+    headers = {"Authorization": api_key, "accept": "application/json"}
+    params = {"take": take, "skip": skip, "answered": "false", "isAnswered": "false"}
+    response = requests.get(f"{WB_API_URL}/api/v1/feedbacks", headers=headers, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error fetching data from Wildberries API. Status code: {response.status_code}, Response: {response.text}")
+        return None
+
+@app.route("/api/v1/feedbacks/<review_id>/reply", methods=["POST"])
+def post_feedback_reply(review_id):#Обрабатывает запрос на отправку ответа на отзыв и вызывает reply_to_review.
+    user_id = request.headers.get("user_id")
+    if not user_id:
+        return jsonify({"error": True, "message": "User_id не предоставлен"}), 400
+
+    api_key = get_api_key(user_id)  # Извлечение API-ключа из базы данных
+    if not api_key:
+        return jsonify({"error": True, "message": "API-ключ не найден для данного user_id"}), 400
+
+    response_text = request.json.get("response")
+    if not response_text:
+        return jsonify({"error": True, "message": "Response is required"}), 400
+
+    result = reply_to_review(api_key, review_id, response_text)
+
+    if result:
+        return jsonify({"message": "Reply sent successfully"})
+    else:
+        return jsonify({"error": True, "message": "Error sending reply"}), 500
+
+def reply_to_review(api_key, review_id, reply_text):#Отправляет ответ на отзыв с использованием API WB и возвращает результат.
     url = "https://feedbacks-api.wildberries.ru/api/v1/feedbacks"
     headers = {
         "Authorization": api_key,
@@ -152,8 +185,10 @@ def reply_to_review(api_key, review_id, reply_text):
         print(f"Ошибка при отправке ответа на отзыв: {response.status_code}, пожалуйста, свяжитесь с поддержкой: @ltroy_sw")
         return None
 
+
+#БЛОК РАБОТЫ С ИИ
 @app.route("/api/v1/generate_response", methods=["POST"])
-def generate_response():
+def generate_response():#принимает запрос на ответ на отзыв от ИИ и вызывает generate_gpt3_response
     prompt = request.json.get("prompt")
     if not prompt:
         return jsonify({"error": True, "message": "Требуется указать текст для генерации ответа"}), 400
@@ -161,7 +196,7 @@ def generate_response():
     response = generate_gpt3_response(prompt)
     return jsonify({"response": response})
 
-def generate_gpt3_response(prompt):
+def generate_gpt3_response(prompt):#генерирует ответ на отзыв от ИИ
     content = f"Ответь на отзыв клиента: {prompt}"
 
     messages = [
@@ -182,30 +217,10 @@ def generate_gpt3_response(prompt):
 
     return response.choices[0].message['content'].strip()
 
-@app.route("/api/v1/feedbacks/<review_id>/reply", methods=["POST"])
-def post_feedback_reply(review_id):
-    user_id = request.headers.get("user_id")
-    if not user_id:
-        return jsonify({"error": True, "message": "User_id не предоставлен"}), 400
-
-    api_key = get_api_key(user_id)  # Извлечение API-ключа из базы данных
-    if not api_key:
-        return jsonify({"error": True, "message": "API-ключ не найден для данного user_id"}), 400
-
-    response_text = request.json.get("response")
-    if not response_text:
-        return jsonify({"error": True, "message": "Response is required"}), 400
-
-    result = reply_to_review(api_key, review_id, response_text)
-
-    if result:
-        return jsonify({"message": "Reply sent successfully"})
-    else:
-        return jsonify({"error": True, "message": "Error sending reply"}), 500
-
-def main():
+#ОСТАЛЬНОЕ
+def main():#проверяет таблицу и запускает сервер
     create_table_if_not_exists()
     app.run(host="localhost", port=12345, threaded=True)
 
-if __name__ == "__main__":
+if __name__ == "__main__":#запускает сервер
     main()
